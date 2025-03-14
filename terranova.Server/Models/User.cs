8ace632ddec378fdc038c29bf09d590f5ad8c665
marcor2742 <marcoruggiero.mail@gmail.com>
2025-03-14 15:14:21 +0100

    start

---
+++ terranova.Server/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace terranova.Server.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Username { get; set; } = "";
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        [StringLength(100, MinimumLength = 8)]
+        public string Password { get; set; } = "";
+
+    }
+}
---
+++ terranova.Server/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using terranova.Server.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.MapStaticAssets();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
