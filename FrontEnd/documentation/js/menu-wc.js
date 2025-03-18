069a7512fd70c018241c4e68bdf2526cdcc17f39
Lorenzo <150276837+Bombatomica64@users.noreply.github.com>
2025-03-18 17:55:09 +0100

    Login (#1)
    * Add initial Angular application structure with routing and styles
    
    * Add user authentication services and environment configurations
    
    * Add documentation styles, scripts, and environment configurations
    
    * Add UI components and configuration for Spartan UI library
    
    * Refactor styles and update button component for improved responsiveness and visual consistency

---
+++ FrontEnd/documentation/js/menu-wc.js
@@ -0,0 +1,208 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">front-end documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-bs-toggle="collapse" ${ isNormalMode ?
+                                'data-bs-target="#modules-links"' : 'data-bs-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">Modules</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                        </a>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/HlmButtonModule.html" data-type="entity-link" >HlmButtonModule</a>
+                                <li class="chapter inner">
+                                    <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ?
+                                        'data-bs-target="#directives-links-module-HlmButtonModule-351538ac21b22b5b8cb9a0b2d8f313d1632feaf566c4700d6df804bf217a596e69db7453f0fa96d34aca38a010ab6c54c8c9929d0a12183be12922fef95a1a51"' : 'data-bs-target="#xs-directives-links-module-HlmButtonModule-351538ac21b22b5b8cb9a0b2d8f313d1632feaf566c4700d6df804bf217a596e69db7453f0fa96d34aca38a010ab6c54c8c9929d0a12183be12922fef95a1a51"' }>
+                                        <span class="icon ion-md-code-working"></span>
+                                        <span>Directives</span>
+                                        <span class="icon ion-ios-arrow-down"></span>
+                                    </div>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="directives-links-module-HlmButtonModule-351538ac21b22b5b8cb9a0b2d8f313d1632feaf566c4700d6df804bf217a596e69db7453f0fa96d34aca38a010ab6c54c8c9929d0a12183be12922fef95a1a51"' :
+                                        'id="xs-directives-links-module-HlmButtonModule-351538ac21b22b5b8cb9a0b2d8f313d1632feaf566c4700d6df804bf217a596e69db7453f0fa96d34aca38a010ab6c54c8c9929d0a12183be12922fef95a1a51"' }>
+                                        <li class="link">
+                                            <a href="directives/HlmButtonDirective.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >HlmButtonDirective</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </li>
+                </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ButtonComponent.html" data-type="entity-link" >ButtonComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ConfirmationModalComponent.html" data-type="entity-link" >ConfirmationModalComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LoginPageComponent.html" data-type="entity-link" >LoginPageComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MyUiComponent.html" data-type="entity-link" >MyUiComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#directives-links"' :
+                                'data-bs-target="#xs-directives-links"' }>
+                                <span class="icon ion-md-code-working"></span>
+                                <span>Directives</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="directives-links"' : 'id="xs-directives-links"' }>
+                                <li class="link">
+                                    <a href="directives/HlmButtonDirective.html" data-type="entity-link" >HlmButtonDirective</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#classes-links"' :
+                            'data-bs-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/User.html" data-type="entity-link" >User</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/LoginService.html" data-type="entity-link" >LoginService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/MyUiService.html" data-type="entity-link" >MyUiService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/UserGetterService.html" data-type="entity-link" >UserGetterService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/AuthResponse.html" data-type="entity-link" >AuthResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/BrnButtonConfig.html" data-type="entity-link" >BrnButtonConfig</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ButtonClickEvent.html" data-type="entity-link" >ButtonClickEvent</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/PartialUserResponse.html" data-type="entity-link" >PartialUserResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/UserExist.html" data-type="entity-link" >UserExist</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/UserGetter.html" data-type="entity-link" >UserGetter</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
---
+++ FrontEnd/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+	production: false,
+	authUrl: 'localhost:8000',
+	loginUrl: 'localhost:8000/login',
+	registerUrl: 'localhost:8000/register',
+	userInfoUrl: 'localhost:8000/user/me',
+	userCheckerUrl: 'localhost:8000/user',
+};
