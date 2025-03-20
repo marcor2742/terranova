a464b79f3a0ee62df18e4b06a449f234262a4de9
Bombatomica64 <qualcosa2001@gmail.com>
2025-03-20 12:43:51 +0100

    removed old frontend

--- FrontEnd/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-	production: true,
-	authUrl: 'localhost:8000',
-	loginUrl: 'localhost:8000/login',
-	registerUrl: 'localhost:8000/register',
-	userInfoUrl: 'localhost:8000/user/me',
-	userCheckerUrl: 'localhost:8000/user',
-};
