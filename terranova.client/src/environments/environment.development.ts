0f9dcd36ac5f04fa8a09fa08ad10b5d47c137143
Bombatomica64 <qualcosa2001@gmail.com>
2025-03-20 12:39:57 +0100

    united the frontend

---
+++ terranova.client/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  authUrl: '/auth',
+  loginUrl: '/login',
+  registerUrl: '/register',
+  userInfoUrl: '/user/me',
+  userCheckerUrl: '/user',
+};
