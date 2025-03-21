23a95916736ed6b5d92e6238029cd9d34e01afde
marcor2742 <marcoruggiero.mail@gmail.com>
2025-03-22 00:08:37 +0100

    refactoring and reordering of Program.cs in other files

---
+++ terranova.Server/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using terranova.Server.Models;
+
+namespace terranova.Server.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerExplorer(this IServiceCollection services)
+        {
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+
+        public static WebApplication ConfigureSwaggerExplorer(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            return app;
+        }
+    }
+}
