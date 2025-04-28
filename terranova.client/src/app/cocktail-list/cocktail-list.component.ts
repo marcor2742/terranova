645f5693d44cfbb44f52dc2646de5133ce115dc4
Bombatomica64 <qualcosa2001@gmail.com>
2025-04-28 16:52:14 +0200

    Refactor cocktail list component: update template structure and enhance imports

--- terranova.client/src/app/cocktail-list/cocktail-list.component.ts
+++ terranova.client/src/app/cocktail-list/cocktail-list.component.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ScrollerModule } from 'primeng/scroller';
+import { DividerModule } from 'primeng/divider';
+import { SkeletonModule } from 'primeng/skeleton';
+import { CocktailCardComponent } from '../cocktail-card/cocktail-card.component';
+import { Cocktail } from '../Classes/cocktail';
 
 @Component({
   selector: 'app-cocktail-list',
-  imports: [CommonModule,TranslateModule],
+  imports: [CommonModule, TranslateModule, ScrollerModule, DividerModule, SkeletonModule, CocktailCardComponent],
   templateUrl: './cocktail-list.component.html',
   styleUrl: './cocktail-list.component.scss'
 })
@@ -16,4 +21,10 @@ export class CocktailListComponent {
 	}
 	searchOrList = input<string>('search');
 	cockTailList = input<number[]>([]);
+	removeCock = output<number>();
+
+	removeCocktail(cocktailId: number) {
+		this.removeCock.emit(cocktailId);
+	}
+
 }
